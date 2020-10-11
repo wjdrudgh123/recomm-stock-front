d841ae0bf3b717559dbe4c831cdf46bf065379c1
Jung-Jaeho <wjdrudgh123@naver.com>
2020-10-11 22:44:52 +0900

    make env

--- src/App.js
+++ src/App.js
@@ -11,13 +11,10 @@ class App extends React.Component {
 
   getLists = async () => {
     console.log("start");
-    const { data } = await axios.get(
-      "https://recommandserver.herokuapp.com/data",
-      {
-        timeout: 300000,
-        withCredentials: "include",
-      }
-    );
+    const { data } = await axios.get(`${process.env.REACT_APP_IP}/data`, {
+      timeout: 300000,
+      withCredentials: "include",
+    });
     console.log(data);
     this.setState({ isLoading: false, companies: data });
   };
