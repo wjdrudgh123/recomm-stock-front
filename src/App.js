ced555cf3493a49057dbc6b032438f174d82014a
Jung-Jaeho <wjdrudgh123@naver.com>
2020-10-23 00:11:35 +0900

    layout change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import axios from "axios";
 import Companies from "./Companies";
 import "./App.css";
-import News from "./News";
 
 class App extends React.Component {
   state = {
@@ -10,20 +9,16 @@ class App extends React.Component {
     companies: [],
     targetCompany: "",
   };
-
+  //`https://${process.env.REACT_APP_IP}
   getLists = async () => {
-    const { data } = await axios.get(
-      `https://${process.env.REACT_APP_IP}/data`,
-      {
-        timeout: 300000,
-        withCredentials: "include",
-      }
-    );
-    const initNews = data[0].companyName;
+    const { data } = await axios.get(`http://localhost:4000/data`, {
+      timeout: 300000,
+      withCredentials: "include",
+    });
+    console.log(data);
     this.setState({
       isLoading: false,
       companies: data,
-      targetCompany: initNews,
     });
   };
 
@@ -31,27 +26,8 @@ class App extends React.Component {
     this.getLists();
   }
 
-  getTargetCompany = (evt) => {
-    let target = evt.target;
-    if (target.className !== "companies__company") {
-      target = target.parentNode;
-      if (
-        target.className === "company__box" ||
-        target.className === "company__title"
-      ) {
-        target = target.parentNode;
-      } else if (target.className === "box__big_box") {
-        target = target.parentNode.parentNode;
-      } else if (target.className === "box__small_box") {
-        target = target.parentNode.parentNode.parentNode;
-      }
-      const companyName = target.dataset.name;
-      this.setState({ targetCompany: companyName });
-    }
-  };
-
   render() {
-    const { isLoading, companies, targetCompany } = this.state;
+    const { isLoading, companies } = this.state;
 
     return (
       <div className="App">
@@ -62,24 +38,12 @@ class App extends React.Component {
         ) : (
           <>
             <div className="App layout">
-              <div className="top_layout">
-                {companies.map(({ companyName, boxPrice }, index) => {
+              <div className="content">
+                {companies.map(({ name, lowPrice }, index) => {
                   return (
-                    <Companies
-                      name={companyName}
-                      boxPrice={boxPrice}
-                      handleEvnet={this.getTargetCompany}
-                      key={index}
-                    />
+                    <Companies name={name} lowPrice={lowPrice} key={index} />
                   );
                 })}
-              </div>
-              <div className="bottom_layout">
-                {companies
-                  .filter(({ companyName }) => companyName === targetCompany)
-                  .map(({ news }, key) => {
-                    return <News name={targetCompany} news={news} key={key} />;
-                  })}
               </div>
             </div>
           </>
--- src/Companies.js
+++ src/Companies.js
@@ -1,27 +1,18 @@
 import React from "react";
 
-const Companies = ({ name, boxPrice, handleEvnet }) => {
-  const { firstBox, secBox } = boxPrice;
-  const bigBoxReg = String(secBox[1]).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  const bigBoxSup = String(secBox[0]).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  const smallBoxReg = String(firstBox[1]).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  const smallBoxSup = String(firstBox[0]).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+const Companies = ({ name, lowPrice }) => {
+  const supportPrice = String(lowPrice).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
-    <div className="companies" onClick={handleEvnet}>
-      <div className="companies__company" data-name={name}>
+    <div className="companies">
+      <div className="companies__company">
         <div className="company__title">
           <h3>{name}</h3>
         </div>
         <div className="company__box">
-          <div className="box__big_box">
-            <span className="big_box__reg">{bigBoxReg}</span>
-            <div className="box__small_box">
-              <span className="small_box__reg">{smallBoxReg}</span>
-              <span className="small_box__sup">{smallBoxSup}</span>
-            </div>
-            <span className="big_box__sup">{bigBoxSup}</span>
-          </div>
+          <span className="box__check_1"></span>
+          <span className="box__check_2"></span>
+          <h4 className="supportPrice">{supportPrice}</h4>
         </div>
       </div>
     </div>
